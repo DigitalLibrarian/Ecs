44b0af3afdb99b41c66f4b488532f5ea922c291f
DigitalLibrarian <likwid38225@gmail.com>
2022-05-14 20:29:48 -0500

    Initial Import
    Broke out re-useable entity component systems project into its own solution.

---
+++ Ecs/Ecs/IEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecs
+{
+    public interface IEntity
+    {
+        int Id { get; }
+        bool HasComponent(int componentId);
+        void AddComponent(IComponent comp);
+        TComponent GetComponent<TComponent>(int componentId);
+    }
+}
---
+++ Ecs/Ecs/IEntityManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecs
+{
+    public interface IEntityManager
+    {
+        IEntity CreateEntity();
+        IEntity CreateEntity(int entityId);
+
+        IEntity GetEntity(int entityId);
+        IEntity DeleteEntity(int entityId);
+        IEnumerable<IEntity> GetEntities(IEnumerable<int> componentIds);
+    }
+}
